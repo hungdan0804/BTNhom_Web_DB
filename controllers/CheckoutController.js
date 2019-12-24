281b7f7cfd8e73bc21742e026560c2d65fd5a726
316324 <hungdan123456@gmail.com>
2019-12-24 13:09:44 +0700

    [Fix bug checkout]

--- controllers/CheckoutController.js
+++ controllers/CheckoutController.js
@@ -2,19 +2,20 @@ const Bill = require('../Models/BillModel');
 const BillDetail = require('../Models/BillDetailModel');
 let CryptoJS = require("crypto-js");
 exports.Checkout = (req, res, next) => {
-    if (req.user == undefined || req.user == null)
+    if (req.user === undefined)
         res.redirect('/user/login');
     res.render('checkout', { title: 'Express' });
 };
 exports.CheckoutSuccess = (req, res, next) => {
-    if (req.user == undefined || req.user == null)
+    if (req.user === undefined)
         res.redirect('/user/login');
     res.render('checkout_success', { title: 'Checked out' });
 };
 exports.CheckoutPOST = (req, res, next) => {
-    res.redirect('/checkout/success');
-    if (req.user == undefined || req.user == null)
+    // res.redirect('/checkout/success');
+    if (req.user === undefined) {
         res.redirect('/');
+    }
     let user_id = req.user.user_id;
     let form = req.body;
     let cart = CryptoJS.AES.decrypt(form.cart,'checkout').toString(CryptoJS.enc.Utf8);
